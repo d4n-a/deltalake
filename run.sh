82e3a627e8cc0def3c0e9d46e5a35bcc23e07476
Danil Astakhov <vo0xr0c@pror0c.local>
2021-05-11 22:16:29 +0300

    Run.sh added

---
+++ run.sh
@@ -0,0 +1,8 @@
+rm -rf checkpoints datasource delta
+mkdir -p delta/events delta/events_processed datasource
+
+echo "run python serv"
+
+spark-submit --packages io.delta:delta-core_2.12:0.8.0,org.apache.spark:spark-sql-kafka-0-10_2.12:3.0.1 --conf "spark.sql.extensions=io.delta.sql.DeltaSparkSessionExtension" --conf "spark.sql.catalog.spark_catalog=org.apache.spark.sql.delta.catalog.DeltaCatalog" consumer_stocks.py
+ 
+spark-submit --packages io.delta:delta-core_2.12:0.8.0,org.apache.spark:spark-sql-kafka-0-10_2.12:3.0.1 --conf "spark.sql.extensions=io.delta.sql.DeltaSparkSessionExtension" --conf "spark.sql.catalog.spark_catalog=org.apache.spark.sql.delta.catalog.DeltaCatalog" consumer_stocks.py
