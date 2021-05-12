163ec092fd7aa24e1e7b62097ff51fe413a27eec
Danil Astakhov <vo0xr0c@pror0c.local>
2021-05-12 22:52:48 +0300

    refactoring: remove unused code

--- csv_stream_starter.py
+++ csv_stream_starter.py
@@ -1,56 +1,6 @@
-import random
-
-
-template = {
-    'trans_id', 'sender_id', 'sender_info', 'recv_id', 'recv_info', 'amount'
-}
-
-def generate_trans_id():
-    return random.randint(1000, 100000)
-
-def generate_sender_id():
-    return random.getrandbits(128)
-
-def generate_sender_info(trans_id):
-    return "Some sender values for trans_id={}".format(trans_id)
-
-def generate_recv_id():
-    return random.getrandbits(128)
-
-def generate_recv_info(trans_id):
-    return "Some recv values for trans_id={}".format(trans_id)
-
-def generate_amount(trans_id):
-    return str(trans_id)[:3]
-
-
-def generate_event():
-    trans_id = generate_trans_id()
-    event = (
-        ('trans_id', trans_id),
-        ('sender_id', generate_sender_id()),
-        ('sender_info', generate_sender_info(trans_id)),
-        ('recv_id', generate_recv_id()),
-        ('recv_info', generate_recv_info(trans_id)),
-        ('amount', generate_amount(trans_id)),
-    )
-    return event
-
-import pprint
-pprint.pprint(generate_event())
-
-
-import socket
+import csv
 import time
 import random
-import csv
-from collections.abc import Callable
-
-
-HOST = '127.0.0.1'
-PORT = 9999
-WORDS_IN_SENTENCE = 10
-
 
 class Source():
     def __init__(self, filename):
@@ -64,22 +14,19 @@ class Source():
     def read(self, strings_amount):
             new_pos = self.pos + strings_amount
             data = self.data[self.pos:new_pos]
-            print(f'{data=}')
             self.pos = new_pos
-            print(f'{self.pos=}')
             return '\n'.join([','.join(row) for row in data])
 
-import csv
 
 def produce_file(source, dir, number):
     with open(dir + '/source' + str(number) + '.csv', 'w', newline='') as wf:
         data = source.read(random.randint(5, 15)).strip('\n')
         # data = source.read(1).strip('\n')
         print(f'{len(data)=}')
-        print(f'{data=}')
+        # print(f'{data=}')
         wf.write(data)
 
-def run_files():
+def start_csv_stream():
     idx = 0
     source = Source("datasource/all_stocks_5yr.csv")
     while True:
@@ -90,4 +37,4 @@ def run_files():
         time.sleep(wait_time)
 
 if __name__ == '__main__':
-    run_files()
+    start_csv_stream()
