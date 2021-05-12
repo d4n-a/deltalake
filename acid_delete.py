f7d91071183724772c49b09b5151fed2f78632bc
Danil Astakhov <vo0xr0c@pror0c.local>
2021-05-12 04:17:28 +0300

    working delete

---
+++ acid_delete.py
@@ -0,0 +1,18 @@
+import pyspark
+import pyspark.sql.functions as F
+
+from delta.tables import *
+
+spark = pyspark.sql.SparkSession.builder.master('spark://127.0.0.1:7077').appName("MyApp") \
+    .config("spark.jars.packages", "io.delta:delta-core_2.12:0.8.0") \
+    .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
+    .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog") \
+    .config("spark.sql.streaming.forceDeleteTempCheckpointLocation", True) \
+    .getOrCreate()
+
+deltaTable = DeltaTable.forPath(spark, "delta/processed/")
+
+deltaTable.delete(F.abs(F.col("delta_total_percents")) > 2)   # predicate using Spark SQL functions
+
+deltaTable.toDF().select('*').where(F.col("delta_total_percents") > 1.7).show()
+
