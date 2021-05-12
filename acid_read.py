cea27fb56265f7d89f05f1d10c6a18a5a0e3b87f
Danil Astakhov <vo0xr0c@pror0c.local>
2021-05-12 05:54:29 +0300

    refactor to unify func

--- acid_read.py
+++ acid_read.py
@@ -4,14 +4,16 @@ import pyspark.sql.types as stypes
 
 from delta.tables import *
 
-spark = pyspark.sql.SparkSession.builder.master('spark://127.0.0.1:7077').appName("MyApp") \
-    .config("spark.jars.packages", "io.delta:delta-core_2.12:0.8.0") \
-    .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
-    .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog") \
-    .config("spark.sql.streaming.forceDeleteTempCheckpointLocation", True) \
-    .getOrCreate()
+def show_df(df, dtp_threshold):
+    df.select('*').where(F.col("delta_total_percents") > dtp_threshold).show()
 
-processed = DeltaTable.forPath(spark, "delta/processed/")
+if __name__ == '__main__':
+    spark = pyspark.sql.SparkSession.builder.master('spark://127.0.0.1:7077').appName("MyApp") \
+        .config("spark.jars.packages", "io.delta:delta-core_2.12:0.8.0") \
+        .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
+        .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog") \
+        .config("spark.sql.streaming.forceDeleteTempCheckpointLocation", True) \
+        .getOrCreate()
 
-DTP_THRESHOLD = 2
-processed.toDF().select('*').where(F.col("delta_total_percents") > DTP_THRESHOLD).show()
+    processed = DeltaTable.forPath(spark, "delta/processed/")
+    show_df(processed.toDF(), 2)
